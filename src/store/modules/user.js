a9c533c318d19a4140d1b2622e2e2f9914e6848f
hosizoraru <hosizoraru@gmail.com>
2023-12-27 10:59:43 +0800

    完善login

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,11 +2,17 @@ export default {
   namespaced: true, // 封闭小仓库 全局->局部
   state: {
     // username: '张三'
+    token: localStorage.getItem('token') ? localStorage.getItem('token') : ''
   },
   mutations: {
     // changeName(state, newName) {
     //   state.username = newName
     // }
+    // 更新仓库的 token 数据
+    setToken(state, newToken) {
+      state.token = newToken
+      localStorage.setItem('token', newToken)
+    }
   },
   actions: {}
 }
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -24,7 +24,7 @@
           label="密码"
           prop="password"
         >
-          <el-input v-model="form.password" @change="checkRememberPassword"/>
+          <el-input v-model="form.password" type="password" @change="checkRememberPassword"/>
         </el-form-item>
 
         <!--        <el-form-item-->
@@ -42,6 +42,33 @@
           <el-button class="login_btn" type="primary" @click="login">登录</el-button>
         </el-form-item>
       </el-form>
+
+      <!-- <el-form ref="form2" :model="form2" :rules="rules2">-->
+      <!-- <el-form-item-->
+      <!--    label="用户名"-->
+      <!--    prop="username2"-->
+      <!--    >-->
+      <!--          <el-input v-model="form2.username2" />-->
+      <!--        </el-form-item>-->
+
+      <!--        <el-form-item-->
+      <!--          label="银行卡号"-->
+      <!--          prop="cardId"-->
+      <!--        >-->
+      <!--          <el-input v-model="form2.cardId" />-->
+      <!--        </el-form-item>-->
+
+      <!--        <el-form-item-->
+      <!--          label="密码"-->
+      <!--          prop="password2"-->
+      <!--        >-->
+      <!--          <el-input v-model="form2.password2" />-->
+      <!--        </el-form-item>-->
+
+      <!--        <el-form-item>-->
+      <!--          <el-button class="login_btn" type="primary" @click="login2">显示存款</el-button>-->
+      <!--        </el-form-item>-->
+      <!--      </el-form>-->
     </div>
   </div>
 </template>
@@ -71,13 +98,41 @@ export default {
           { required: true, message: '密码必须填写', trigger: 'blur' },
           { min: 6, max: 12, message: '密码长度必须在 6 - 12 位', trigger: 'blur' }
         ]
+        // },
+        // form2: {
+        //   username2: '',
+        //   cardId: '',
+        //   password2: ''
+        // },
+        // rules2: {
+        //   username2: [
+        //     { required: true, message: '用户名必须填写', trigger: 'blur' }
+        //   ],
+        //   cardId: [
+        //     { required: true, message: '银行卡号必须填写', trigger: 'blur' },
+        //     { min: 16, max: 16, message: '银行卡号长度必须为 16 位', trigger: 'blur' }
+        //   ],
+        //   password2: [
+        //     { required: true, message: '密码必须填写', trigger: 'blur' },
+        //     { min: 6, message: '密码长度必须在 6 位以上', trigger: 'blur' }
+        //   ]
       }
     }
   },
+  // 生命周期函数 => 特定时机触发
+  // mounted => 页面加载完成后触发
   mounted() {
     const username = localStorage.getItem('username')
     const password = localStorage.getItem('password')
-    const remember = localStorage.getItem('remember') === 'true'
+    const remember = JSON.parse(localStorage.getItem('remember'))
+    // const remember = localStorage.getItem('remember') === 'true'
+    // const temp = localStorage.getItem('form')
+    // if (temp) {
+    //   this.form = JSON.parse(temp) // 不知道是否可用 需要验证
+    //   this.form.username = JSON.parse(temp).username
+    //   this.form.password = JSON.parse(temp).password
+    //   this.remember = true
+    // }
     if (!remember) {
       localStorage.removeItem('username')
       localStorage.removeItem('password')
@@ -101,6 +156,15 @@ export default {
             loginAPI(this.form).then(
               res => {
                 console.log(res.data.token)
+                // 如何调用仓库的 mutation 函数
+                // this.$store.commit('模块名/mutation名', 参数) vuex 提供的一个属性 指向仓库本身
+                // setToken(res.data.token) // 这不对
+                this.$store.commit('user/setToken', res.data.token)
+                // this.$router.push('/') 跳转页面 比如首页 vuex 提供的一个属性 指向路由实例
+                this.$router.push('/')
+                // this.$message elementUI 提供的一个属性 指向一个对象 身上有很多提示的方法
+                this.$message.success('恭喜登陆成功')
+                // this.$message.error('登陆失败')
                 // console.log(this.remember)
                 if (this.remember) {
                   this.saveUserInfo(
@@ -108,6 +172,7 @@ export default {
                     this.form.password,
                     this.remember
                   )
+                  // localStorage.setItem('form', JSON.stringify(this.form))
                 } else {
                   localStorage.removeItem('username')
                   localStorage.removeItem('password')
@@ -122,13 +187,28 @@ export default {
       //   res => console.log(res.data.token)
       // )
     },
+    // login2() {
+    //   this.$refs.form2.validate(
+    //     ok => {
+    //       if (ok) {
+    //         console.log(this.form2)
+    //       }
+    //     }
+    //   )
+    // },
+    // 前端本地存储
+    // localStorage 永久存储
+    // sessionStorage 关闭页面后就没了
+    // cookie 永久/有效期内存储 比较灵活 但是容量小 一般适用于数据少
+    // localStorage setItem getItem removeItem clear
+    // JSON.stringify() JSON.parse()
     saveUserInfo(username, password, remember) {
       localStorage.setItem('username', username)
       localStorage.setItem('password', password)
-      localStorage.setItem('remember', String(remember))
+      localStorage.setItem('remember', JSON.stringify(remember))
     },
     saveRememberState(remember) {
-      localStorage.setItem('remember', String(remember))
+      localStorage.setItem('remember', JSON.stringify(remember))
       if (remember) {
         this.saveUserInfo(this.form.username, this.form.password, remember)
       }
