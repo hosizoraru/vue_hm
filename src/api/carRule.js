463c4c757d586d396daa251686d9eca133bea8a4
hosizoraru <hosizoraru@gmail.com>
2024-01-03 09:55:14 +0800

    计费管理的添加删除刷新

---
+++ src/api/carRule.js
@@ -0,0 +1,58 @@
+/*
+    计费详情接口
+ */
+
+import request from '@/utils/request'
+
+/**
+ * 获取规则列表
+ * @param { page, pageSize} params
+ * @returns
+ */
+export function getRuleListAPI(params) {
+  return request({
+    url: 'parking/rule/list',
+    params
+  })
+}
+
+export function getRuleDropListAPI() {
+  return request({
+    url: 'parking/rule/dropList',
+    method: 'GET'
+  })
+}
+
+/**
+ * 创建规则
+ * @returns
+ */
+export function createRuleAPI(data) {
+  return request({
+    url: '/parking/rule',
+    method: 'POST',
+    data
+  })
+}
+
+export function getRuleDetailAPI(id) {
+  return request({
+    url: `/parking/rule/${id}`,
+    method: 'GET'
+  })
+}
+
+export function updateRuleAPI(data) {
+  return request({
+    url: '/parking/rule',
+    method: 'PUT',
+    data
+  })
+}
+
+export function deleteRuleAPI(id) {
+  return request({
+    url: `/parking/rule/${id}`,
+    method: 'DELETE'
+  })
+}
--- src/views/Car/CarRule/index.vue
+++ src/views/Car/CarRule/index.vue
@@ -1,3 +1,383 @@
 <template>
-  <div>行车管理-规则管理</div>
+  <div class="rule-container">
+    <div class="create-container">
+      <el-button-group>
+        <el-button type="primary" @click="openDialog">增加停车计费规则</el-button>
+        <el-button plain @click="exportToExcel">导出Excel</el-button>
+        <el-button type="success" icon="el-icon-refresh" :loading="loadingFlag" size="medium" @click="refreshLoading">刷新页面</el-button>
+      </el-button-group>
+    </div>
+    <!-- 表格区域 -->
+    <div class="table">
+      <el-table
+        v-loading="loadingFlag"
+        element-loading-text="拼命加载中"
+        element-loading-spinner="el-icon-loading"
+        element-loading-background="rgba(0, 0, 0, 0.3)"
+        :data="ruleList"
+        style="width: 100%"
+      >
+        <el-table-column type="index" label="序号" />
+        <el-table-column label="计费规则编号" width="120px" prop="ruleNumber" />
+        <el-table-column label="计费规则名称" width="120px" prop="ruleName" />
+        <el-table-column label="免费时长(分钟)" width="120px" prop="freeDuration" />
+        <el-table-column label="收费上限(元)" width="120px" prop="chargeCeiling" />
+        <el-table-column label="计费方式" width="100px">
+          <template #default="scope">
+            {{ formatChargeType(scope.row.chargeType) }}
+          </template>
+        </el-table-column>
+        <el-table-column label="计费规则" prop="ruleNameView" width="120px" />
+        <el-table-column label="操作" fixed="right">
+          <template #default="scope">
+            <el-button-group>
+              <el-button size="mini" type="text">查看</el-button>
+              <el-button size="mini" type="text">编辑</el-button>
+              <el-button size="mini" type="text" @click="deleteRule(scope.row.id)">删除</el-button>
+            </el-button-group>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <el-dialog
+      :visible.sync="dialogVisible"
+      width="50%"
+      title="新增规则"
+      class="dialog-container"
+      :before-close="handleClose"
+      center
+    >
+      <!-- 表单接口 -->
+      <div class="form-container">
+        <el-form ref="addForm" :model="addForm" :rules="addFormRules" label-position="top">
+          <el-form-item label="计费规则编号" prop="ruleNumber">
+            <el-input v-model="addForm.ruleNumber" />
+          </el-form-item>
+          <el-form-item label="计费规则名称" prop="ruleName">
+            <el-input v-model="addForm.ruleName" />
+          </el-form-item>
+          <el-form-item label="计费方式" prop="chargeType">
+            <el-radio-group v-model="addForm.chargeType" size="small">
+              <el-radio label="duration" border>时长收费</el-radio>
+              <el-radio label="turn" border>按次收费</el-radio>
+              <el-radio label="partition" border>分段收费</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <div class="flex-container">
+            <el-form-item label="免费时长" prop="freeDuration">
+              <el-input v-model="addForm.freeDuration" />
+            </el-form-item>
+            <el-form-item label="收费上限" prop="chargeCeiling">
+              <el-input v-model="addForm.chargeCeiling" />
+            </el-form-item>
+          </div>
+          <el-form-item label="计费规则">
+            <!-- 按时长收费区域 -->
+            <div v-if="addForm.chargeType === 'duration'" class="duration">
+              每 <el-input v-model="addForm.durationTime" class="input-box" /> 小时 <el-input v-model="addForm.durationPrice" class="input-box" /> 元
+            </div>
+            <!-- 按次收费区域 -->
+            <div v-if="addForm.chargeType === 'turn'" class="turn">
+              每次 <el-input v-model="addForm.turnPrice" class="input-box" /> 元
+            </div>
+            <!-- 按分段收费区域 -->
+            <div v-if="addForm.chargeType==='partition'" class="partition">
+              <div class="item"><el-input v-model="addForm.partitionFrameTime" class="input-box" />小时内,每小时收费<el-input v-model="addForm.partitionFramePrice" class="input-box" /> 元</div>
+              <div class="item">每增加<el-input v-model="addForm.partitionIncreaseTime" class="input-box" />小时,增加<el-input v-model="addForm.partitionIncreasePrice" class="input-box" /> 元</div>
+            </div>
+          </el-form-item>
+        </el-form>
+      </div>
+      <template #footer>
+        <el-button-group>
+          <el-button size="medium" type="warning" @click="clearForm">重置</el-button>
+          <el-button size="medium" type="danger" @click="handleClose">取 消</el-button>
+          <el-button size="medium" type="primary" @click="confirmAdd">确 定</el-button>
+        </el-button-group>
+      </template>
+    </el-dialog>
+  </div>
 </template>
+
+<script>
+import { createRuleAPI, deleteRuleAPI, getRuleListAPI } from '@/api/carRule'
+import { utils, writeFileXLSX } from 'xlsx'
+
+export default {
+  name: 'Building',
+  data() {
+    return {
+      ruleList: [],
+      params: {
+        page: 1,
+        pageSize: 10
+      },
+      total: 0,
+      dialogVisible: false,
+      loadingFlag: true,
+      addForm: {
+        ruleNumber: '', // 计费规则编号
+        ruleName: '', // 计费规则名称
+        chargeType: 'duration', // 计费规则类型 duration / turn / partition
+        chargeCeiling: null,
+        freeDuration: null,
+        // 时长计费独有字段
+        durationTime: null, // 时长计费单位时间
+        durationPrice: null, // 时长计费单位价格
+        durationType: 'hour',
+        // 按次收费独有字段
+        turnPrice: null,
+        // 分段计费独有字段
+        partitionFrameTime: null, // 段内时间
+        partitionFramePrice: null, // 段内费用
+        partitionIncreaseTime: null, // 超出时间
+        partitionIncreasePrice: null // 超出费为收费价钱
+      }, // 计费规则表单对象
+      addFormRules: {
+        ruleNumber: [
+          {
+            required: true,
+            message: '请输入规则编号',
+            trigger: 'blur'
+          }
+        ],
+        ruleName: [
+          {
+            required: true,
+            message: '请输入规则名称',
+            trigger: 'blur'
+          }
+        ],
+        chargeType: [
+          {
+            required: true,
+            message: '请选择收费类型',
+            trigger: 'blur'
+          }
+        ],
+        freeDuration: [
+          {
+            required: true,
+            message: '请输入免费时长',
+            trigger: 'blur'
+          }
+        ],
+        chargeCeiling: [
+          {
+            required: true,
+            message: '请输入收费上限',
+            trigger: 'blur'
+          }
+        ]
+      } // 计费规则校验规则对象
+    }
+  },
+  async mounted() {
+    await this.getList()
+    // const dropList = await getRuleDropListAPI()
+    // console.dir(dropList)
+  },
+  methods: {
+    async getList() {
+      const res = await getRuleListAPI(this.params)
+      this.loadingFlag = false
+      this.ruleList = res.data.rows
+      this.total = res.data.total
+    },
+    // 导出excel
+    async exportToExcel() {
+      // 获取要导出的业务数据
+      const res = await getRuleListAPI(this.params)
+      // 表头英文字段key
+      const tableHeaderKeys = ['ruleNumber', 'ruleName', 'freeDuration', 'chargeCeiling', 'chargeType', 'ruleNameView']
+      // 表头中文字段value
+      const tableHeaderValues = ['计费规则编号', '计费规则名称', '免费时长(分钟)', '收费上线(元)', '计费方式', '计费规则']
+      // 以excel表格的顺序调整后端数据
+      // 适配excel表格
+      const sheetData = res.data.rows.map(item => {
+        const _obj = {}
+        tableHeaderKeys.forEach(key => {
+          // 赋值
+          // 针对计费规则做处理
+          if (key === 'chargeType') {
+            _obj[key] = this.formatChargeType(item[key])
+          } else {
+            _obj[key] = item[key]
+          }
+        })
+        return _obj
+      })
+      // 创建一个工作表
+      const worksheet = utils.json_to_sheet(sheetData)
+      // 创建一个新的工作簿
+      const workbook = utils.book_new()
+      // 把工作表添加到工作簿
+      utils.book_append_sheet(workbook, worksheet, 'Data')
+      // 改写表头
+      utils.sheet_add_aoa(worksheet, [tableHeaderValues], { origin: 'A1' })
+      writeFileXLSX(workbook, '计费规则.xlsx')
+    },
+    formatChargeType(type) {
+      const TypeMAP = {
+        'duration': '按时长收费',
+        'turn': '按次收费',
+        'partition': '分段收费'
+      }
+      return TypeMAP[type]
+    },
+    openDialog() {
+      this.dialogVisible = true
+    },
+    closeDialog() {
+      this.dialogVisible = false
+    },
+    clearForm() {
+      this.$refs.addForm.resetFields()
+    },
+    async refreshLoading() {
+      this.loadingFlag = true
+      await this.getList()
+    },
+    handleClose() {
+      this.$confirm('确认关闭？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+        center: true
+      })
+        .then(
+          () => {
+            this.closeDialog()
+            this.clearForm()
+          }
+        ).catch(
+          () => {
+            this.$message({
+              type: 'info',
+              message: '已取消关闭'
+            })
+          }
+        )
+    },
+    confirmAdd() {
+      this.$refs.addForm.validate(
+        async valid => {
+          if (valid) {
+            await createRuleAPI(this.addForm).then(
+              async() => {
+                await this.getList()
+                this.$message({
+                  type: 'success',
+                  message: '添加成功'
+                })
+                this.closeDialog()
+                this.clearForm()
+              }
+            ).catch(
+              error => {
+                this.$message({
+                  type: 'error',
+                  message: error.response.data.msg
+                })
+              }
+            )
+          } else {
+            this.$message({
+              type: 'error',
+              message: '添加失败'
+            })
+            return false
+          }
+        }
+      )
+    },
+    deleteRule(id) {
+      this.$confirm('确认删除？')
+        .then(
+          async() => {
+            await deleteRuleAPI(id).then(
+              async() => {
+                await this.getList()
+                this.$message({
+                  type: 'success',
+                  message: '删除成功'
+                })
+              }
+            ).catch(
+              error => {
+                this.$message({
+                  type: 'error',
+                  message: error.response.data.msg
+                })
+              }
+            )
+          }
+        ).catch(
+          () => {
+            this.$message({
+              type: 'info',
+              message: '已取消删除'
+            })
+          }
+        )
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.rule-container {
+  padding: 20px;
+  background-color: #fff;
+}
+
+.search-container {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgb(237, 237, 237, .9);
+  padding-bottom: 20px;
+
+  .search-label {
+    width: 100px;
+  }
+
+  .search-main {
+    width: 220px;
+    margin-right: 10px;
+  }
+}
+
+.create-container {
+  margin: 10px 0;
+}
+
+.page-container {
+  padding: 4px 0;
+  text-align: right;
+}
+
+.form-container {
+  padding: 20px 80px;
+}
+
+.dialog-container {
+  max-height: 100vh; /* 设置最大高度，以免对话框过长 */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.flex-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.input-box {
+  width: 100px; /* 调整输入框的宽度，可以根据需要进行更改 */
+}
+
+/* 重置、取消和确定按钮的样式 */
+.el-button-group .el-button {
+  margin: 0 5px; /* 调整按钮之间的间距 */
+}
+</style>
